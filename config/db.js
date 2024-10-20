833b7e9ad2dfd6309c039774a6341e4d328198bd
Prateek <codes4prateek@gmail.com>
2024-10-20 20:32:56 +0530

    fixing

--- config/db.js
+++ config/db.js
@@ -1,18 +1,21 @@
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI;
-if (!uri) {
-    console.error('MONGODB_URI is not defined in the environment variables');
-    process.exit(1);
-}
-    await mongoose.connect(process.env.MONGODB_URI);
+    if (!uri) {
+      console.error('MONGODB_URI is not defined in the environment variables');
+      process.exit(1);
+    }
+
+    // Print the MONGODB_URI value to verify it
+    console.log('MONGODB_URI:', uri);
+
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('Failed to connect to MongoDB:', err.message);
     process.exit(1);
   }
 };
